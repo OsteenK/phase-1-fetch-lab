105459843563ba1ef6aa0c4866c5eb81f9cac1d1
OsteenK <osteen.mutinda@student.moringaschool.com>
2022-12-02 00:26:37 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((json) => renderBooks(json))
 }
 
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+	 -12,6 +15,6 ;
+    function renderBooks(books) {}}); 
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks();
+});
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
